6f61ac0e898354a88cf71d151ed72c7ca8aba8f1
admin <admin@adminui-MacBookPro.local>
2026-03-07 13:13:19 -0800

    tmdb 수정

--- src/api/tmdb.js
+++ src/api/tmdb.js
@@ -11,5 +11,19 @@ export const getPopularMovies = async () => {
   );
 
   const data = await response.json();
-  return data.results;
+  return data;
+};
+
+export const getMovieDetail = async (id) => {
+  const response = await fetch(
+    `https://api.themoviedb.org/3/movie/${id}`,
+    {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    }
+  );
+
+  const data = await response.json();
+  return data;
 };
