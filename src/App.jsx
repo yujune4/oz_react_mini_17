e12337082d86952617794d1ae8bcd7e38244b2d3
admin <admin@adminui-MacBookPro.local>
2026-03-06 18:57:23 -0800

    App.jsx 수정

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,13 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Layout from "./components/Layout";
+import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
-import MovieDetail from "./pages/MovieDetail"; // wrapper 사용
+import MovieDetail from "./pages/MovieDetail";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-         <Route path="/" element={<Home />} />
-         <Route path="/movie/:id" element={<MovieDetail />} />
-        </Routes>
-      </BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/movie/:id" element={<MovieDetail />} />
+    </Routes>
   );
 }
 
