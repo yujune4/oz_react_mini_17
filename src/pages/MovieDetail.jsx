0c87b3ad46979560f152ed1aac6e9a7341c2a820
admin <admin@adminui-MacBookPro.local>
2026-03-06 18:52:27 -0800

    MovieDetail.jsx 수정

--- src/pages/MovieDetail.jsx
+++ src/pages/MovieDetail.jsx
@@ -1,13 +1,13 @@
 import { useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
-import { getMovieDeatail } from "../api/tmdb.js";
+import { getMovieDetail } from "../api/tmdb";
 
 function MovieDetail() {
   const { id } = useParams();
   const [movie, setMovie] = useState(null);
 
   useEffect(() => {
-    getMovieDeatail(id).then(data => setMovie(data));
+    getMovieDetail(id).then(data => setMovie(data));
   }, [id]);
 
   if (!movie) return <div>Loading...</div>;
@@ -15,13 +15,7 @@ function MovieDetail() {
   return (
     <div>
       <h1>{movie.title}</h1>
-      <img
-        src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
-        alt={movie.title}
-      />
       <p>{movie.overview}</p>
-      <p>평점: {movie.vote_average}</p>
-      <p>개봉일: {movie.release_date}</p>
     </div>
   );
 }
