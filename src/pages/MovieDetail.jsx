87a8d4f84ffb19ef0a2819675c3186b831c84b46
admin <admin@adminui-MacBookPro.local>
2026-03-06 23:40:53 -0800

    MovieDetail 수정

--- src/pages/MovieDetail.jsx
+++ src/pages/MovieDetail.jsx
@@ -1,20 +1,44 @@
 import { useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
-import { getMovieDetail } from "../api/tmdb";
+import { getMovieDetail } from "../api/tmdb.js";
 
 function MovieDetail() {
-  const { id } = useParams();
+  const { id } = useParams();          
   const [movie, setMovie] = useState(null);
 
   useEffect(() => {
-    getMovieDetail(id).then(data => setMovie(data));
+    const fetchMovie = async () => {
+      const data = await getMovieDetail(id);
+      setMovie(data);
+    };
+
+    fetchMovie();
   }, [id]);
 
-  if (!movie) return <div>Loading...</div>;
+  if (!movie) {
+    return <div>Loading...</div>;
+  }
 
   return (
-    <div>
+    <div style={{ padding: "20px" }}>
       <h1>{movie.title}</h1>
+
+      <img
+        src={`https://image.tmdb.org/t/p/w300${movie.poster_path}`}
+        alt={movie.title}
+      />
+
+      <p>
+        <strong>평점:</strong> ⭐ {movie.vote_average}
+      </p>
+
+      <p>
+        <strong>개봉일:</strong> {movie.release_date}
+      </p>
+
+      <p>
+        <strong>줄거리:</strong>
+      </p>
       <p>{movie.overview}</p>
     </div>
   );
