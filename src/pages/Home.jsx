8e797d421eb74753367a650e337bd1a0889056e3
admin <admin@adminui-MacBookPro.local>
2026-03-07 13:15:44 -0800

    Home 수정

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,19 +1,20 @@
 import { useEffect, useState } from "react";
 import MovieCard from "../components/MovieCard";
-import { getPopularMovies } from "../api/tmdb.js";
+import { getPopularMovies } from "../api/tmdb";
 
 function Home() {
   const [movies, setMovies] = useState([]);
 
- useEffect(() => {
-  getPopularMovies().then(data => {
-    setMovies(data.results);
-  });
-}, []);
-  
+  useEffect(() => {
+    getPopularMovies().then((data) => {
+      setMovies(data.results);
+    });
+  }, []);
+
   return (
     <div>
       <h1>영화 목록</h1>
+
       <div className="movie-list">
         {movies.map((movie) => (
           <MovieCard key={movie.id} movie={movie} />
