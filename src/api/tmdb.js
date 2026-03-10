cf60f7b16621b4a16fdcaf0b4c55534677904495
admin <admin@adminui-MacBookPro.local>
2026-03-10 15:23:50 -0700

    tmdb 수정

--- src/api/tmdb.js
+++ src/api/tmdb.js
@@ -2,7 +2,7 @@ const token = import.meta.env.VITE_TMDB_API_TOKEN;
 
 export const getPopularMovies = async () => {
   const response = await fetch(
-    "https://api.themoviedb.org/3/movie/popular",
+    "https://api.themoviedb.org/3/movie/popular?language=ko&page=1&region=KR",
     {
       headers: {
         Authorization: `Bearer ${token}`,
@@ -16,7 +16,7 @@ export const getPopularMovies = async () => {
 
 export const getMovieDetail = async (id) => {
   const response = await fetch(
-    `https://api.themoviedb.org/3/movie/${id}`,
+    `https://api.themoviedb.org/3/movie/${id}?language=ko&page=1&region=KR`,
     {
       headers: {
         Authorization: `Bearer ${token}`,
