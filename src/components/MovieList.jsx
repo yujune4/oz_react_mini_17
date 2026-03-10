44da9cdf56ac9dc7d779aca2e7dd82529d5aa38e
yujune4 <yujunsin4@gmail.com>
2026-03-10 18:26:41 +0900

    MovieList 수정

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import { useSearchParams } from "react-router-dom";
+import { getPopularMovies, searchMovies } from "../api/tmdb";
 
 function MovieList() {
 
@@ -7,32 +8,26 @@ function MovieList() {
   const [searchParams] = useSearchParams();
   const query = searchParams.get("query");
 
-  const token = import.meta.env.VITE_TMDB_API_TOKEN;
-
   useEffect(() => {
 
-    const url = query
-      ? `https://api.themoviedb.org/3/search/movie?query=${query}&language=ko-KR`
-      : "https://api.themoviedb.org/3/movie/popular?language=ko-KR&page=1";
+    const fetchMovies = async () => {
 
-    fetch(url, {
-      method: "GET",
-      headers: {
-        accept: "application/json",
-        Authorization: `Bearer ${token}`
+      let data;
+
+      if (query) {
+        data = await searchMovies(query);
+      } else {
+        data = await getPopularMovies();
       }
-    })
-      .then((res) => res.json())
-      .then((data) => {
 
-        const filteredMovies = data.results.filter(
-          (movie) => movie.adult === false
-        );
+      const filteredMovies = data.results.filter(
+        (movie) => movie.adult === false
+      );
 
-        setMovies(filteredMovies);
+      setMovies(filteredMovies);
+    };
 
-      })
-      .catch((error) => console.error(error));
+    fetchMovies();
 
   }, [query]);
 
