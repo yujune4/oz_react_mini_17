4fdaf7a809cd04111fd7140b00f464900b53b877
yujune4 <yujunsin4@gmail.com>
2026-03-10 17:50:52 +0900

    MovieList 수정

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,45 +1,52 @@
 import { useEffect, useState } from "react";
+import { useSearchParams } from "react-router-dom";
 
 function MovieList() {
 
-    const [movies, setMovies] = useState([]);
+  const [movies, setMovies] = useState([]);
+  const [searchParams] = useSearchParams();
+  const query = searchParams.get("query");
 
-    const token = import.meta.env.VITE_TMDB_API_TOKEN;
+  const token = import.meta.env.VITE_TMDB_API_TOKEN;
 
-    useEffect(() => {
+  useEffect(() => {
 
-        fetch("https://api.themoviedb.org/3/movie/popular", {
-          method: "GET",
-          headers: {
-            accept: "application/json",
-            Authorization: `Bearer ${token}`
-          }
-        })
-          .then((res) => res.json())
-          .then((data) => {
+    const url = query
+      ? `https://api.themoviedb.org/3/search/movie?query=${query}&language=ko-KR`
+      : "https://api.themoviedb.org/3/movie/popular?language=ko-KR&page=1";
 
-            const filteredMovies = data.results.filter(
-                (movie) => movie.adult === false
-            );
+    fetch(url, {
+      method: "GET",
+      headers: {
+        accept: "application/json",
+        Authorization: `Bearer ${token}`
+      }
+    })
+      .then((res) => res.json())
+      .then((data) => {
 
-            setMovies(filteredMovies);
+        const filteredMovies = data.results.filter(
+          (movie) => movie.adult === false
+        );
 
-          })
-          .catch((error) => console.error(error));
+        setMovies(filteredMovies);
 
-    }, []);
+      })
+      .catch((error) => console.error(error));
 
-    return (
-      <div>
-        <h2>Popular Movies</h2>
+  }, [query]);
 
-        {movies.map((movie) => (
-          <div key={movie.id}>
-            {movie.title}
-          </div> 
-        ))}
-      </div>
-    )
+  return (
+    <div>
+      <h2>{query ? `Search Result: ${query}` : "Popular Movies"}</h2>
+
+      {movies.map((movie) => (
+        <div key={movie.id}>
+          {movie.title}
+        </div>
+      ))}
+    </div>
+  );
 }
 
 export default MovieList;
