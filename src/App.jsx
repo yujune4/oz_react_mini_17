7d026e3c963dd4b0affd5b2098df2a8261b39c8b
yujune4 <yujunsin4@gmail.com>
2026-03-11 10:49:15 +0900

    App 수정

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,21 @@
-import { Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
-import MovieDetail from "./pages/MovieDetail";
-import Layout from "./components/Layout";
+import Detail from "./pages/Detail";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
 
 function App() {
   return (
-    <Routes>
-      <Route element={<Layout />}>
+    <Router>
+      <Navbar />
+      <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/movie/:id" element={<MovieDetail />} />
-      </Route>
-    </Routes>
+        <Route path="/detail/:id" element={<Detail />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+      </Routes>
+    </Router>
   );
 }
 
