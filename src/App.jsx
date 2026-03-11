0c21a5bdd67aa78f9d2960a1c8cdd6080ec70c38
yujune4 <yujunsin4@gmail.com>
2026-03-11 15:48:32 +0900

    App 수정

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,21 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Navbar from "./components/Navbar";
+import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Detail from "./pages/Detail";
-import Login from "./pages/Login";
-import Signup from "./pages/Signup";
+import NavBar from "./components/NavBar";
 
-function App() {
+export default function App() {
   return (
-    <>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/detail/:id" element={<Detail />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<Signup />} />
-      </Routes>
-    </>
+    <div className="min-h-screen">
+      {/* 1. NavBar를 여기에 둡니다. */}
+      <NavBar /> 
+
+      {/* 2. NavBar가 상단 고정(fixed)이라면 pt-20을 줘서 본문을 아래로 내려야 합니다. */}
+      <main className="pt-20"> 
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/detail/:id" element={<Detail />} />
+        </Routes>
+      </main>
+    </div>
   );
 }
-
-export default App;
